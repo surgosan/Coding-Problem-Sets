817c9a9be44745ef18772fb23057613ec9617543
surgosan <sergy200118@gmail.com>
2024-05-24 19:13:24 -0400

    JavaScript Two Sum Problem Comments

--- JavaScript Coding Problems/Easy/Two Sum Problem.js
+++ JavaScript Coding Problems/Easy/Two Sum Problem.js
@@ -7,29 +7,29 @@
 
 //---------------------------------------------------- BRUTE FORCE ----------------------------------------------------
 function bruteForceMethod(nums, target) {
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = 0; j < nums.length; j++) {
-            if(i === j) {
+    for(let i = 0; i < nums.length; i++) {      // Iterate over each num
+        for(let j = 0; j < nums.length; j++) { // Iterate each time every num
+            if(i === j) {                               // If the indices are the same, skip it
                 continue;
             }
-            if(nums[i] + nums[j] === target) {
+            if(nums[i] + nums[j] === target) {// If the sum equals the target, return it
                 return [i, j];
             }
         }
     }
-    return null;
+    return null; // Return null if no answer found
 }
 //------------------------------------------------------- HASHMAP -------------------------------------------------------
 function hashMapMethod(nums, target) {
-    const map = new Map();
-    for(let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if(map.has(complement)) {
-            return [map.get(complement), i];
+    const map = new Map();                          // Create new Map
+    for(let i = 0; i < nums.length; i++) {          // Iterate over nums
+        const complement = target - nums[i];    // Create the complement number
+        if(map.has(complement)) {                   // Check if the complement already exists
+            return [map.get(complement), i];       // If it does, return it
         }
-        map.set(nums[i], i);
+        map.set(nums[i], i);                            // On each iterate, add the current number and it's index
     }
-    return null;
+    return null; // Return null if no answer found
 }
 //---------------------------------------------------- MAIN METHOD ----------------------------------------------------
 const nums = [2,7,11,15];
