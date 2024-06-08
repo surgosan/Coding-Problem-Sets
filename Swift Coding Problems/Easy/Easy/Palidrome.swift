c97600e5238f9513256fcb4571d9ae2a86b81188
Sergio S.A <surgosan@gmail.com>
2024-06-07 21:20:37 -0400

    Swift Palidrome Problem

--- Swift Coding Problems/Easy/Easy/Palidrome.swift
+++ Swift Coding Problems/Easy/Easy/Palidrome.swift
@@ -5,4 +5,61 @@
 //  Created by Sergio Sanchez-Alvares on 6/6/24.
 //
 
-import Foundation
+// --------------------------------------------- BRUTE FORCE O(n) ---------------------------------------------
+func palidromeBruteForce(_ num: Int) -> Bool {
+    var num: Int = num // Create another variable called num to make it mutable
+    // Negative numbers are not palidromes
+    if(num < 0) { return false }
+    
+    // Store the original number
+    let originalNum: Int = num
+    var reversedNum: Int = 0
+    
+    // Reverse the integer
+    while(num != 0) {
+        let digit = num % 10 // Borrow the last digit
+        reversedNum = reversedNum*10 + digit // Add a 0 to the end (*10) and add the digit we removed to that slot
+        num /= 10 // Divide num by 10 to remove the last digit
+    }
+    
+    // Check if the reversed number is equal to the original number
+    return originalNum == reversedNum
+}
+
+func palidromeSemiReverse(_ num: Int) -> Bool {
+    var num: Int = num // Create another variable called num to make it mutable
+    // num cannot be negative | num cannot end in 0, but does not count if num is 0 (It would be a palidrome them (difference between 10 and 0))
+    if(num < 0 || (num % 10 == 0 && num != 0)) { return false }
+    
+    // Initialize reverted number
+    var revertedNumber = 0
+    while(num > revertedNumber) {
+        /*  1. Multiply by 10 to gain a 0 (121 * 10 = 1210)
+                1a. Done on reverted number to prepare to add a new digit in that empty slot
+            2. mod(%) original number by 10 (121 % 10 = 1)
+                2a. This is done to get the last digit of the original number
+            3. Divide(/) original number by 10
+                3a. This is done to move the decimal to the left 1 digit. (121 / 10 = 12)
+                3b. Since the type is integer, it will round down to nearest whole number
+         */
+        revertedNumber = revertedNumber * 10 + (num % 10)
+        num /= 10
+    }
+    /*
+        This method does not reverse the complete number.
+        EX: In 12321, at the end of the loop we get x=12 and revertedNum=123 because x is now less than the reverted num
+            EXa. This only happens if there are an odd number of digits, otherwise, x and revertedNum will equal each-other
+        This is fine. Simply divide(/) by 10 to remove the last digit.
+        Now the comparison is 12 == 12. The middle digit does not matter since the middle digit equals itself
+
+        EVEN DIGITS EXAMPLE
+            1221 -> x=12  revertedNum=12
+            x == 12 ✓
+        ODD DIGITS EXAMPLE
+            12321 -> x = 12  revertedNum == 123
+            revertedNum / 10 = 12
+            x == revertedNum
+    */
+    
+    return num == revertedNumber || num == revertedNumber / 10
+}
--- Swift Coding Problems/Easy/Easy/main.swift
+++ Swift Coding Problems/Easy/Easy/main.swift
@@ -8,24 +8,41 @@
 import Foundation
 
 //-------------------------------------------------- TWO SUM ---------------------------------------------------
-let twoSumNums = [2,7,11,15]
-let twoSumTarget = 9
-let twoSumNumsTwo = [3,2,4]
-let twoSumTargetTwo = 6
-let twoSumNumsThree = [3,3]
-let twoSumTargetThree = 6
+print("--------------------- Two Sum Problem ---------------------\n")
+let twoSumNums: [Int] = [2,7,11,15]
+let twoSumTarget: Int = 9
+let twoSumNumsTwo: [Int] = [3,2,4]
+let twoSumTargetTwo: Int = 6
+let twoSumNumsThree: [Int] = [3,3]
+let twoSumTargetThree: Int = 6          // Dummy Data
 
 let twoSumBruteOne = twoSumBruteMethod(twoSumNums, twoSumTarget)
 let twoSumBruteTwo = twoSumBruteMethod(twoSumNumsTwo, twoSumTargetTwo)
 let twoSumBruteThree = twoSumBruteMethod(twoSumNumsThree, twoSumTargetThree)
 print(String(format: "Two Sum Brute Force 1 [2,7,11,15]:  [%d, %d]", twoSumBruteOne[0], twoSumBruteOne[1]))
 print(String(format: "Two Sum Brute Force 2 [3,2,4]:  [%d, %d]", twoSumBruteTwo[0], twoSumBruteTwo[1]))
-print(String(format: "Two Sum Brute Force 3 [3,3]:  [%d, %d]\n\n", twoSumBruteThree[0], twoSumBruteThree[1]))
+print(String(format: "Two Sum Brute Force 3 [3,3]:  [%d, %d]\n", twoSumBruteThree[0], twoSumBruteThree[1]))
 
 let twoSumDictOne = twoSumDictionaryMethod(twoSumNums, twoSumTarget)
 let twoSumDictTwo = twoSumDictionaryMethod(twoSumNumsTwo, twoSumTargetTwo)
 let twoSumDictThree = twoSumDictionaryMethod(twoSumNumsThree, twoSumTargetThree)
 print(String(format: "Two Sum Dictionary Method 1 [2,7,11,15]:  [%d, %d]", twoSumDictOne[0], twoSumDictOne[1]))
 print(String(format: "Two Sum Dictionary Method 2 [3,2,4]:  [%d, %d]", twoSumDictTwo[0], twoSumDictTwo[1]))
-print(String(format: "Two Sum Dictionary Method 3 [3,3]:  [%d, %d]", twoSumDictThree[0], twoSumDictThree[1]))
+print(String(format: "Two Sum Dictionary Method 3 [3,3]:  [%d, %d]\n", twoSumDictThree[0], twoSumDictThree[1]))
 
+//-------------------------------------------------- PALIDROME ---------------------------------------------------
+print("--------------------- Palidrome Problem ---------------------\n")
+let palidromeNum1: Int = 121;
+let palidromeNum2: Int = 122;
+let palidromeNum3: Int = 12321;
+let palidromeNum4: Int = 124321;        // Dummy Data
+
+print("Palidrome Brute Force 1 (121): \(palidromeBruteForce(palidromeNum1))")
+print("Palidrome Brute Force 2 (122): \(palidromeBruteForce(palidromeNum2))")
+print("Palidrome Brute Force 3 (12321): \(palidromeBruteForce(palidromeNum3))")
+print("Palidrome Brute Force 4 (124321): \(palidromeBruteForce(palidromeNum4))\n")
+
+print("Palidrome Semi Reversal 1 (121): \(palidromeSemiReverse(palidromeNum1))")
+print("Palidrome Semi Reversal 2 (122): \(palidromeSemiReverse(palidromeNum2))")
+print("Palidrome Semi Reversal 3 (12321): \(palidromeSemiReverse(palidromeNum3))")
+print("Palidrome Semi Reversal 4 (124321): \(palidromeSemiReverse(palidromeNum4))\n")
